ae35b5f11d6a60cd055e618fdca0881e8c948383
Fredrik Löwenhamn <lowet84@gmail.com>
2016-11-18 15:29:53 +0100

    harharhar

--- setup/reset.sh
+++ setup/reset.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
+TOKEN="029fdf.9a2016d3ac853537"
+URL="home.test"
 kubeadm reset
 systemctl start kubelet
-kubeadm init --pod-network-cidr=10.244.0.0/16 --token="029fdf.9a2016d3ac853537" --api-external-dns-names="home.test"
+kubeadm init --pod-network-cidr=10.244.0.0/16 --token=$TOKEN --api-external-dns-names=$URL
 kubectl apply -f flannel.yml
 kubectl taint nodes --all dedicated-
+echo "kubeadm join --token=$TOKEN $URL" > join.sh
