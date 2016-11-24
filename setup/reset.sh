5e96857f2fe30dc02c1ffd27cdf285765e59acf0
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2016-11-24 11:24:38 +0100

    Add Sandbox

--- setup/reset.sh
+++ setup/reset.sh
@@ -11,6 +11,6 @@ kubeadm reset
 systemctl start kubelet
 kubeadm init --pod-network-cidr=10.244.0.0/16 --token=$TOKEN --api-external-dns-names=$URL
 kubectl apply -f flannel.yml
-kubectl taint nodes --all dedicated-
+#kubectl taint nodes --all dedicated-
 echo "writing file join.sh"
 echo "kubeadm join --token=$TOKEN $URL" > join.sh
