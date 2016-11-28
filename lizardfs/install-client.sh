5482fa8e7b33d0f74ca828d800ad3c590d162952
Fredrik Löwenhamn <lowet84@gmail.com>
2016-11-28 14:31:19 +0100

    Create install-client.sh

---
+++ lizardfs/install-client.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget -O - http://packages.lizardfs.com/lizardfs.key | apt-key add -
+echo "deb http://packages.lizardfs.com/ubuntu/xenial xenial main" > /etc/apt/sources.list.d/lizardfs.list
+echo "deb-src http://packages.lizardfs.com/ubuntu/xenial xenial main" >> /etc/apt/sources.list.d/lizardfs.list
+apt-get update
+apt-get install -y lizardfs-client
