b4c0e600a7bdd2d8a43ec37c70fab3ed1cecb01f
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2016-11-30 13:25:51 +0100

    forgot we're not using a smart language

--- lizardfs/install-client.sh
+++ lizardfs/install-client.sh
@@ -4,4 +4,4 @@ wget -O - http://packages.lizardfs.com/lizardfs.key | apt-key add -
 echo "deb http://packages.lizardfs.com/ubuntu/xenial xenial main" > /etc/apt/sources.list.d/lizardfs.list
 echo "deb-src http://packages.lizardfs.com/ubuntu/xenial xenial main" >> /etc/apt/sources.list.d/lizardfs.list
 apt-get update
-apt-get install -y lizardfs-client
+apt-get install -y lizardfs-client jq
