aa959c6a14399c3bb2a179a01736f2ea82410125
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2016-12-06 16:09:25 +0100

    fixed errors in the previous commit

--- setup/reset.sh
+++ setup/reset.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 if [ -z "$1" ]; then
-  echo "Syntax is 'sh reset.sh <url of host>'"
+  echo "Syntax is:"
+  echo "sh reset.sh <url of host>"
+  echo "sh reset.sh <url of host> <token>"
   exit 0
 fi
-FIRST=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1)
-SECOND=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
-TOKEN=$FIRST.$SECOND
+if [ ! -z "$2" ]; then
+  TOKEN=$2
+else
+  FIRST=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1)
+  SECOND=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
+  TOKEN=$FIRST.$SECOND
+fi
 URL=$1
 kubeadm reset
 systemctl start kubelet
