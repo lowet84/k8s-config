a010d883d663928d0b20d0b5f6460a216695e945
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2016-12-07 17:27:02 +0100

    Not one conflict, today was a good day.

--- nginx-proxy/run.sh
+++ nginx-proxy/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker rm -f nginx-kube-proxy
 #sh build.sh
-docker run -d -it --name nginx-kube-proxy --net=host lowet84/nginx-kube-proxy
+docker run -d -it --restart always --name nginx-kube-proxy --net=host lowet84/nginx-kube-proxy
--- setup/reset.sh
+++ setup/reset.sh
@@ -16,7 +16,8 @@ URL=$1
 kubeadm reset
 systemctl start kubelet
 kubeadm init --pod-network-cidr=10.244.0.0/16 --token=$TOKEN --api-external-dns-names=$URL
-kubectl apply -f flannel.yml
+#kubectl apply -f flannel.yml
+kubectl apply -f weave.yml
 #kubectl taint nodes --all dedicated-
 echo "writing file join.sh"
 echo "kubeadm join --token=$TOKEN $URL" > join.sh
