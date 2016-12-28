e2e8972c609176bfdd99c8605b3e644f823d10e7
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2016-12-28 20:43:21 +0000

    things occurred

---
+++ setup/install-rpi.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+apt-get update && apt-get install -y kubeadm
+
+
