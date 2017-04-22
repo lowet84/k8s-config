9db77a65b35a58d3ac3da17b751ed25ffe68badd
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2017-04-22 09:12:13 +0000

    COMMIT ALL THE FILES!

---
+++ setup/reset-arm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ ! -z "$1" ]; then
+  TOKEN=$1
+else
+  FIRST=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 6 | head -n 1)
+  SECOND=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 16 | head -n 1)
+  TOKEN=$FIRST.$SECOND
+fi
+kubeadm reset
+kubeadm init --token=$TOKEN
+
+cp /etc/kubernetes/admin.conf ~/.kube/config
+
+kubectl apply -f weave-arm.yml
+echo "writing file join.txt"
+echo "kubeadm join --token=$TOKEN" > /root/join.txt
