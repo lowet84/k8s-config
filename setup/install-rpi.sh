6c2d09368ae478d632a312574aa018b53e829410
Fredrik Löwenhamn <lowet84@gmail.com>
2017-04-23 12:31:05 +0200

    Update install-rpi.sh

--- setup/install-rpi.sh
+++ setup/install-rpi.sh
@@ -2,6 +2,6 @@
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
-apt-get update && apt-get install -y kubelet=1.5.2-00 kubectl=1.5.2-00 kubeadm=1.5.6-00
+apt-get update && apt-get install -y kubelet=1.5.6-00 kubectl=1.5.6-00 kubeadm=1.5.6-00
 
 
