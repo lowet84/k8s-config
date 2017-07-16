1f57ebab79dcef140f2d3a812d5991cd1f74926d
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2017-07-16 12:10:07 +0200

    jobs... steve jobs

--- apps/core/traefik-kube/prod/prepare.sh
+++ apps/core/traefik-kube/prod/prepare.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 kubectl create configmap traefik-conf --from-file=traefik.toml --namespace=kube-system
+kubectl create configmap traefik-public-conf --from-file=traefik-public.toml --namespace=kube-system
